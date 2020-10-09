88510eca02aadbc8da4456a9498abd689319baa7
Paul Lam <paulhklam1122@gmail.com>
2020-10-08 23:44:21 -0700

    finished app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from './components/Form'
+import Title from './components/Title'
+import Weather from './components/Weather'
+
+const API_KEY = 'aaf9767d18047986612c6149ae3851f3'
+
+class App extends Component {
+  state = {
+    city: null,
+    country: null,
+    humidity: null,
+    description: null,
+    minTemp: null,
+    maxTemp: null,
+    error: null
+  }
+
+  getWeather = async event => {
+    event.preventDefault()
+
+    console.log('Event', event)
+
+    const { city, country } = event.target.elements
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city.value},${country.value}&appid=${API_KEY}&units=metric`
+
+    const api_call = await fetch(url)
+
+    const data = await api_call.json()
+    console.log('data', data)
+
+    if (city && country && data.cod !== '404') {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        minTemp: data.main.temp_min,
+        maxTemp: data.main.temp_max,
+        error: ''
+      })
+    } else if (city && country) {
+      this.setState({
+        city: null,
+        country: null,
+        humidity: null,
+        temperature: null,
+        description: null,
+        minTemp: null,
+        maxTemp: null,
+        error: 'City not found.'
+      })
+    } else {
+      this.setState({
+        city: null,
+        country: null,
+        humidity: null,
+        temperature: null,
+        description: null,
+        minTemp: null,
+        maxTemp: null,
+        error: 'Please fill in the form fields.'
+      })
+    }
+  }
+
+  render() {
+    const {
+      temperature,
+      city,
+      country,
+      humidity,
+      description,
+      minTemp,
+      maxTemp,
+      error
+    } = this.state
+    return (
+      <div className='wrapper'>
+        <div className='main'>
+          <Grid container spacing={0}>
+            <Grid item xs={5}>
+              <Title />
+            </Grid>
+            <Grid item xs={7} className='information-container'>
+              <Form getWeather={this.getWeather} />
+              <Weather
+                temperature={temperature}
+                city={city}
+                country={country}
+                humidity={humidity}
+                description={description}
+                minTemp={minTemp}
+                maxTemp={maxTemp}
+                error={error}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Form.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  textField: {
+    display: 'flex',
+    margin: theme.spacing.unit,
+    width: '50%'
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+})
+
+const Form = props => (
+  <form onSubmit={props.getWeather}>
+    <TextField
+      className={props.classes.textField}
+      label='City Name'
+      margin='normal'
+      name='city'
+      type='search'
+      variant='outlined'
+      InputLabelProps={{
+        required: true,
+        color: 'white',
+        shrink: true
+      }}
+    />
+    <TextField
+      className={props.classes.textField}
+      label='Country Code'
+      margin='normal'
+      name='country'
+      type='search'
+      variant='outlined'
+      InputLabelProps={{
+        required: true,
+        shrink: true
+      }}
+    />
+    <Button
+      className={props.classes.button}
+      color='primary'
+      type='submit'
+      variant='contained'
+    >
+      Get Weather
+    </Button>
+  </form>
+)
+
+export default withStyles(styles)(Form)
